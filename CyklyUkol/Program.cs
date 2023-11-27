349450f4a8b96180374ca84772cc14c8e485be3a
Jadranek <jadranekkubota@gmail.com>
2023-11-27 10:33:42 +0100

    first commit

---
+++ CyklyUkol/Program.cs
@@ -0,0 +1,25 @@
+﻿if (false)
+{
+    bool proceed = true;
+    string result = "";
+    while (proceed)
+    {
+        string userInput = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(userInput) != true)
+        {
+            result = result + userInput;
+            proceed = true;
+        }
+        else
+        {
+            Console.WriteLine(result);
+            proceed = false;
+        }
+    }
+}
+bool cycle = true;
+string userInput = "";
+for (int i = 0; i < max; i++)
+{
+
+}
